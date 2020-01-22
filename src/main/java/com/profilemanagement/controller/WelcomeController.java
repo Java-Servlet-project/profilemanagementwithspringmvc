640654f9bc890d983a32831157546c15712f3d75
soumitradutta <ai.soumitra@gmail.com>
2020-01-22 20:50:35 +0530

    3rd

--- src/main/java/com/profilemanagement/controller/WelcomeController.java
+++ src/main/java/com/profilemanagement/controller/WelcomeController.java
@@ -5,14 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping(value = "/profilemanagement")
 public class WelcomeController {
-	
-	//@GetMapping(value = "/")
-    public String m2() {
-        System.out.println("############# //// ############");
-        return "home";
-    }
     
     @GetMapping(value = "/first")
     public String m1() {
--- src/main/webapp/views/home.jsp
+++ src/main/webapp/views/home.jsp
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
+
 <html>
-<body>
-<h2>Hello World!</h2>
-<a href = "profilemanagement/first">Click</a>
-</body>
+	<body>
+		<p>
+			<h1>Employee Profile Management</h1>
+		</p>
+		
+		<p>
+			<ul>
+				<li><a href = "profilemanagement/first">LogIn</a></li>
+				<li><a href = "profilemanagement/first">My Profile</a></li>
+				<li><a href = "profilemanagement/first">LogOut</a></li>
+			</ul>			
+		</p>
+	</body>
 </html>
